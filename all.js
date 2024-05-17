84106e208b14110ecd9ee692d3ccd00d27807eb5
Yoman666 <ivy410384@gmail.com>
2024-05-17 14:36:37 +0800

    prettier

--- all.js
+++ all.js
@@ -10,13 +10,9 @@ const App = {
         getData() {
             axios.get(`${this.url}/api/Record/GetRecords`)
                 .then(res => {
-
                     console.log(res.data.Data);
-                    // 格式化日期並存回tempData中
                     this.tempData = res.data.Data.map(record => {
-                        // 將日期字串轉換為日期物件
                         const date = new Date(record.DateOfBirth);
-                        // 格式化日期為 "年-月-日"
                         const year = date.getFullYear();
                         const month = (date.getMonth() + 1).toString().padStart(2, '0');
                         const day = date.getDate().toString().padStart(2, '0');
@@ -39,19 +35,17 @@ const App = {
             })
         },
         postData() {
-            // const data = {
-            //     Name: "test",
-            //     DateOfBirth: "2024-05-13T08:27:16.420Z",
-            //     Salary: 0,
-            //     Address: "test2"
-            // };
             axios.post(`http://nexifytw.mynetgear.com:45000/api/Record/SaveRecords`, this.tempData)
                 .then(res => {
                     console.log(res.data);
                     alert('儲存成功');
                     this.getData();
                 })
-                .catch(err => console.log(err))
+                .catch(err => {
+                    console.log(err);
+                    alert('儲存失敗');
+                }
+                )
         }
     },
     mounted() {
